d9b86525f30c44029794740b7b4b3ea19bbd48a2
sjeguedes <sje.guedes@gmail.com>
2020-07-19 14:02:35 +0200

    transferred Doctrine migrations and updated migrations meta data and naming strategy from 'underscore number aware' to 'underscore'

---
+++ migrations/Version20200714195223.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200714195223 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE task CHANGE created_at createdAt DATETIME NOT NULL, CHANGE is_done isDone TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE Task CHANGE createdat created_at DATETIME NOT NULL, CHANGE isdone is_done TINYINT(1) NOT NULL');
+    }
+}
