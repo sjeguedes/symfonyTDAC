08de54d387ba1e7ef08a7e78dbd049972bb7149f
sjeguedes <sje.guedes@gmail.com>
2021-04-13 00:34:39 +0200

    arranged user login form using Symfony form type and logic as expected in UserPasswordFormAuthenticationListener by default

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\Type\LoginUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
@@ -17,40 +17,52 @@ class SecurityController extends AbstractController
      * log in a user.
      *
      * @param AuthenticationUtils $authenticationUtils
-     * @param Request             $request
      *
      * @return Response
      *
-     * @Route("/login", name="login")
+     * @Route("/login", name="login", methods={"GET"})
      */
-    public function loginAction(AuthenticationUtils $authenticationUtils, Request $request)
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
     {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ));
+         return $this->render('security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+            // Create a Symfony form for authentication process
+            'loginUser'     => $this->createForm(LoginUserType::class)->createView()
+        ]);
     }
 
     /**
      * Create a route for guard authentication process.
      *
-     * @Route("/login_check", name="login_check")
+     * @codeCoverageIgnore
+     *
+     * @return void
+     *
+     * @Route("/login_check", name="login_check", methods={"GET", "POST"})
+     *
+     * @throws \Exception
      */
-    public function loginCheck()
+    public function loginCheck(): void
     {
-        // This code is never executed.
+        // This code is never reached.
+        throw new \Exception('This code should never be executed!');
     }
 
     /**
      * Create a route to log out a user.
      *
-     * @Route("/logout", name="logout")
+     * @codeCoverageIgnore
+     *
+     * @return void
+     *
+     * @Route("/logout", name="logout", methods={"GET"})
+     *
+     * @throws \Exception
      */
-    public function logoutCheck()
+    public function logoutCheck(): void
     {
-        // This code is never executed.
+        // This code is never reached.
+        throw new \Exception('This code should never be executed!');
     }
 }
---
+++ src/Form/Type/LoginUserType.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class LoginUserType
+ *
+ * Manage user login action form data.
+ */
+class LoginUserType extends AbstractType
+{
+    /**
+     * Build the form with custom fields.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     *
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        // Define form fields names as expected in UserPasswordFormAuthenticationListener by default
+        $builder
+            ->add('_username', TextType::class, [
+                'label'      => 'Nom d\'utilisateur :'
+            ])
+            ->add('_password', PasswordType::class, [
+                'label'        => 'Mot de passe :'
+            ]);
+    }
+
+    /**
+     * Configure form custom options.
+     *
+     * @param OptionsResolver $resolver
+     *
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Define id as expected in UserPasswordFormAuthenticationListener by default
+            'csrf_token_id' => 'authenticate'
+
+        ]);
+    }
+
+    /**
+     * Cancel form name to return form as expected in UserPasswordFormAuthenticationListener by default.
+     *
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return null;
+    }
+}
